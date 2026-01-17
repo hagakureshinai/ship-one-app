2eb4d7d58ffb60b52478fe448789c30a2c27fc45
hagakureshinai <hagakureshinai@gmail.com>
2026-01-18 06:14:42 +0900

    Localstrage保存

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  function getDoneDays() {
+    const data = localStorage.getItem('doneDays');
+    if (data) {
+      return JSON.parse(data);
+    } else {
+      return [];
+    }
+  }
+
+function saveDoneDays(days) {
+  localStorage.setItem('doneDays', JSON.stringify(days));
+}
+
   const days = document.querySelectorAll('.day');
+  const doneDays = getDoneDays();
+
+  days.forEach(day => {
+    if (doneDays.includes(day.textContent)) {
+      day.classList.add('done');
+    }
+  });
 
   days.forEach(day => {
     day.addEventListener('click', () => {
       day.classList.toggle('done');
-      console.log('ぽん:', day.textContent);
+
+      const dayNumber = day.textContent;
+      let doneDays = getDoneDays();
+
+      if (doneDays.includes(dayNumber)) {
+        doneDays = doneDays.filter(d => d !== dayNumber);
+      } else {
+        doneDays.push(dayNumber);
+      }
+
+      saveDoneDays(doneDays);
     });
+
   });
 });
